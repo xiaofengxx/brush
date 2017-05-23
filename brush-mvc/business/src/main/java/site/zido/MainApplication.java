fc5dea325504518dfa79221832b3c0cb0e357421
zido <wuhongxu1208@gmail.com>
2017-05-23 14:31:49 +0800

    initial project

---
+++ brush-mvc/business/src/main/java/site/zido/MainApplication.java
@@ -0,0 +1,23 @@
+package site.zido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * description:
+ * <p>fill description</p>
+ *
+ * @author zido
+ * @date 2017/5/23 0023 13:46
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MainApplication.class);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 成都顶层刷手后台
+
+* maven 多模块划分
+* 基于spring boot + mybatis
+
+## 项目
+
+* business：商家后台
+* center：中心后台
+* subscriber：用户后台

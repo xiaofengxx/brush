8e882068fb2139ab7651e54d7133b83bc9d1af52
俊桀锅锅 <627438869@qq.com>
2017-06-13 15:52:55 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	doc/项目需求文档.docx

---
+++ center/src/main/java/site/zido/center/web/CenterController.java
@@ -0,0 +1,22 @@
+package site.zido.center.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.zido.core.common.base.BaseController;
+import site.zido.dto.AjaxResult;
+
+/**
+ * Created by CDDC on 2017/6/13.
+ */
+@RestController
+@RequestMapping(value = "/api/center/center")
+public class CenterController extends BaseController{
+    /**
+     * 管理员修改密码
+     */
+    @PostMapping(value = "/updatePwd")
+    public AjaxResult updatePwd(@){
+        return null;
+    }
+}

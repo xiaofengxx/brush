a7f06a1520736642ffef2d063311fc8225e843a3
zido <wuhongxu1208@gmail.com>
2017-06-27 16:44:36 +0800

    完善七牛云上传模块，测试通过

--- other-service/src/main/java/site/zido/otherService/qiniu/QiniuController.java
+++ other-service/src/main/java/site/zido/otherService/qiniu/QiniuController.java
@@ -2,6 +2,7 @@ package site.zido.otherService.qiniu;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.zido.brush.utils.ValiDateUtils;
